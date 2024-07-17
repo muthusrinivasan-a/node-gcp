8dbddfc367b78a952915e6eb4cccf45f8b3c3d10
Muthu <muthusrinivasan.a@gmail.com>
2024-07-17 19:13:08 +0530

    Create swaggerConfig.js

---
+++ swagger/swaggerConfig.js
@@ -0,0 +1,22 @@
+const swaggerJsdoc = require('swagger-jsdoc');
+
+const swaggerOptions = {
+  swaggerDefinition: {
+    openapi: '3.0.0',
+    info: {
+      title: 'Google Calendar API',
+      version: '1.0.0',
+      description: 'API to fetch and store Google Calendar events',
+    },
+    servers: [
+      {
+        url: 'http://localhost:3000',
+      },
+    ],
+  },
+  apis: ['./routes/*.js'], // Path to the API docs
+};
+
+const swaggerDocs = swaggerJsdoc(swaggerOptions);
+
+module.exports = swaggerDocs;
